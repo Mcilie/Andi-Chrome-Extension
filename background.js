65aeece80de46dc29fcfaaa4ae48d1479bfef3be
Michael Ilie <mcilieg@gmail.com>
2022-12-05 20:18:24 -0500

    Cleaning up code

--- background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set(
-    { url: "https://andisearch.com/nav/?query={searchTerms}" },
-    function () {
-      console.log("Default search engine set to Andi Search.");
-    }
-  );
-});
